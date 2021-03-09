24d3fb07ae839241db334d243b8416d20fc4f1e6
Karim <aksharif@umich.edu>
2021-03-09 12:56:56 -0500

    third commit

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,8 +2,8 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 20;
-        int second = 25;
+        int first = 75;
+        int second = 35;
 
         System.out.println("Enter two numbers: " + first + " " + second);
         int sum = first + second;
