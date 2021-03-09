be464da3e7cdcadc4a936e73358a1a194a0360bf
Karim <aksharif@umich.edu>
2021-03-09 12:35:59 -0500

    secondcommit

--- AddIntegers.java
+++ AddIntegers.java
@@ -2,7 +2,7 @@ public class AddIntegers {
 
     public static void main(String[] args) {
 
-        int first = 15;
+        int first = 20;
         int second = 25;
 
         System.out.println("Enter two numbers: " + first + " " + second);
